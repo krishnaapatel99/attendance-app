20fa42eda3b11beb2019a6734a83d64e5adb2481
Krishna Patel <patelkrishu1608@gmail.com>
2026-01-31 22:51:42 +0530

    env updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://attendance-app-backend-4.onrender.com/api",
+    "https://attendance-app-backend-jdny.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
