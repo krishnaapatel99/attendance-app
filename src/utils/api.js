3c11cfeca6eded82948e56f631df1c396dc3b659
Krishna Patel <patelkrishu1608@gmail.com>
2026-02-01 23:29:37 +0530

    custom domain added

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://attendance-app-backend-jdny.onrender.com/api",
+    "https://api.upasthit.in/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
